18d77f088ea677af5fdca0eafa58e2e5518f2082
Alkoun-web-design <hassam.sajid.dvlpr@gmail.com>
2025-08-24 03:54:00 +0500

    fixed stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,20 @@
 import { DateTime } from "luxon"; 
 
 export default async function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('./src/assets');
+    // eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('src/assets');
     
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
         
     return {
-	markdownTemplateEngine: 'njk',
-	dataTemplateEngine: 'njk',
-	htmlTemplateEngine: 'njk',
-	dir: {
-		input: 'src',
-        output: "_site",
-	},
-};
-    return {
         dir: {
             input: "src",
-
-        }
+        },
+        templateFormats: [ "md", "njk", "html", ],
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
     }
 }
